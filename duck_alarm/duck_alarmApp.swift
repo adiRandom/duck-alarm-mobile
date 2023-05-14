60f44d0308223d88fc22b0ace40a50902919347c
adi_random <me.adi.hf@gmail.com>
2023-05-14 21:27:01 +0300

    Initial Commit

---
+++ duck_alarm/duck_alarmApp.swift
@@ -0,0 +1,20 @@
+//
+//  duck_alarmApp.swift
+//  duck_alarm
+//
+//  Created by Adrian Pascu on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct duck_alarmApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
